9da45a9ee56718e8f31970e195389e323c4b0d87
MariaAksonova <mashaaks@ukr.net>
2017-07-27 23:45:55 +0300

    Add welcome message

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var TelegramBot = require('node-telegram-bot-api'),
-    token = '420737343:AAEFUaIA3R6vnycu7Yd9p76n_qGXOTMKf2g';
+const TelegramBot = require('node-telegram-bot-api');
+const token = '420737343:AAEFUaIA3R6vnycu7Yd9p76n_qGXOTMKf2g';
+const welcomeMessage = 'Приветствую! Я FestUaBot. Чем вам помочь?';
 
-var bot = new TelegramBot(token, {
+const bot = new TelegramBot(token, {
     polling: true,
+});
+
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, welcomeMessage);
 });
 
 bot.on('message', function(msg) {
     var id = msg.from.id;
-    bot.sendMessage(id, msg.text)
+    bot.sendMessage(id, msg.text);
 });
