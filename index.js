4e87a0eeb6ee4b547efba8d809bba8fd7d4ddf8f
YarmolenkoDany <yarmolenko.dany@gmail.com>
2017-07-28 17:01:55 +0700

    start development of bot

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 var TelegramBot = require('node-telegram-bot-api'),
     token = '420737343:AAEFUaIA3R6vnycu7Yd9p76n_qGXOTMKf2g';
+    // menu = require('./bot/menu.js');
 
 var bot = new TelegramBot(token, {
-    polling: true,
+    polling: true
 });
 
-bot.on('message', function(msg) {
-    var id = msg.from.id;
-    bot.sendMessage(id, msg.text)
+var chatId;
+
+bot.on('message', function (msg) {
+    chatId = msg.from.id;
+    //bot.sendMessage(id, msg.text);
+    switch (msg.text) {
+        case '/start':
+            start();
+            break;
+        case 'ІНФО':
+            info();
+            break;
+    }
 });
+
+// Эта функция начинает разговор с пользователем
+function start() {
+    bot.sendMessage(chatId, 'Привіт :) Мене звати BotFestUa');
+    bot.sendMessage(chatId, 'Я допоможу тобі забронювати білети на фестиваль, ' +
+        'а також трохи розповім про нього.');
+    menu();
+}
+
+// Эта функция отправляет пользователю две кнопки главного меню
+function menu() {
+    bot.sendMessage(chatId, 'P.S. Якщо хочеш, щоб у нас з тобою все класно склалося, ' +
+        'будь ласка, скористуйся пунктами меню ;)', {
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['ІНФО'],
+                ['Забронювати білет']
+            ]
+        })
+    });
+}
+// Эта функция вызывает менб информации про фест
+
+function info() {
+    bot.sendMessage(chatId, 'Інформація про фест...')
+}
